6d4c0aeefc07b216151262f6cabc2c57576b88cb
Austen-s <189764837+Austen-s@users.noreply.github.com>
2026-05-20 10:21:32 -0400

    CardPanel.java

--- CardPanel.java
+++ CardPanel.java
@@ -14,14 +14,13 @@ class CardPanel extends JPanel {
 
     public CardPanel() {
         setPreferredSize(new Dimension(120, 180));
-        setOpaque(false); // Make background transparent so rounded corners work
+        setOpaque(false); 
         this.isFaceDown = true;
     }
 
-    // Updates the card to be displayed and repaints the panel
     public void setCard(Card card) {
         this.card = card;
-        this.isFaceDown = (card == null); // If null, draw it face down (card back)
+        this.isFaceDown = (card == null); 
         repaint(); 
     }
 
@@ -30,24 +29,20 @@ class CardPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         
-        // Turn on anti-aliasing for smooth rounded edges
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         int width = getWidth();
         int height = getHeight();
 
-        // Draw the white card base with rounded corners
         g2.setColor(Color.WHITE);
         g2.fillRoundRect(0, 0, width - 1, height - 1, 15, 15);
 
-        // Draw a dark border around the card
         g2.setColor(Color.DARK_GRAY);
         g2.setStroke(new BasicStroke(2));
         g2.drawRoundRect(0, 0, width - 1, height - 1, 15, 15);
 
         if (isFaceDown) {
-            // Draw a beautiful Casino-style pattern for the card back
-            g2.setColor(new Color(178, 34, 34)); // Firebrick Red
+            g2.setColor(new Color(178, 34, 34)); 
             g2.fillRoundRect(5, 5, width - 11, height - 11, 10, 10);
             
             g2.setColor(Color.WHITE);
@@ -56,21 +51,17 @@ class CardPanel extends JPanel {
             for(int i = 10; i < height; i += 10) g2.drawLine(5, i, width - 6, i);
             
         } else if (card != null) {
-            // Draw the Card Face (e.g., "A ♥")
             g2.setColor(card.getColor());
 
-            // Top-left small rank and symbol
             g2.setFont(new Font("Arial", Font.BOLD, 20));
             g2.drawString(card.getRank(), 10, 25);
             g2.drawString(card.getSymbol(), 10, 45);
 
-            // Bottom-right small rank and symbol (upside down illusion)
             FontMetrics fm = g2.getFontMetrics();
             int rankWidth = fm.stringWidth(card.getRank());
             g2.drawString(card.getRank(), width - rankWidth - 10, height - 30);
             g2.drawString(card.getSymbol(), width - rankWidth - 10, height - 10);
 
-            // Center Giant Symbol
             g2.setFont(new Font("Arial", Font.PLAIN, 60));
             FontMetrics fmBig = g2.getFontMetrics();
             int symWidth = fmBig.stringWidth(card.getSymbol());
