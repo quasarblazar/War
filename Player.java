14144c8a56bc6f64090a41be16a60f6f59da8f4c
Austen-s <189764837+Austen-s@users.noreply.github.com>
2026-05-20 10:22:51 -0400

    Player.java

--- Player.java
+++ Player.java
@@ -9,8 +9,6 @@ import java.util.LinkedList;
 import java.util.List;
 class Player {
     private final String name;
-    // We use a LinkedList because it acts like a real deck of cards.
-    // We can pull from the top (first) and add to the bottom (last).
     private final LinkedList<Card> hand;
 
     public Player(String name) {
@@ -20,17 +18,14 @@ class Player {
 
     public String getName() { return name; }
 
-    // Adds a single card to the bottom of the player's stack
     public void addCardToBottom(Card card) {
         hand.addLast(card);
     }
 
-    // Adds a pile of won cards to the bottom of the stack
     public void addCardsToBottom(List<Card> wonCards) {
         hand.addAll(wonCards);
     }
 
-    // Removes and returns the top card. Returns null if out of cards.
     public Card drawTopCard() {
         return hand.isEmpty() ? null : hand.pollFirst();
     }
